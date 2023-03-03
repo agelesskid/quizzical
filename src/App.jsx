0d6dd7dda62ed8a0cf7c2346240668a0fdbf27fe
ageless <agelesskidxd@gmail.com>
2023-03-03 02:41:08 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+    <main>
+      <img src="./public/images/big-yellow-blob.png" id='big-yellow-blob' alt="Big yellow blob" />
+      <img src="./public/images/big-blue-blob.png" id='big-blue-blob' alt="Big blue blob" />
+      <h1>Quizzical</h1>
+      <button type='button'>Start quiz</button>
+    </main>
+  )
+}
