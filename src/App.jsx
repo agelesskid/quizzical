17b629337524454d2e260076c51cd9c15000334a
ageless <agelesskidxd@gmail.com>
2023-03-03 03:21:39 +0100

    added new component; made small tweaks
    added Quiz component & styles to it; tweaked styles for button; changed z-index of background images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Start from './components/Start'
+import Quiz from './components/Quiz'
 
 export default function App() {
 
   return (
     <main>
-      <Start />
+      {/* <Start /> */}
+      <Quiz />
     </main>
   )
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,7 +4,7 @@ export default function Start() {
             <img src="/images/big-yellow-blob.png" id='big-yellow-blob' alt="Big yellow blob" />
             <img src="/images/big-blue-blob.png" id='big-blue-blob' alt="Big blue blob" />
             <h1>Quizzical</h1>
-            <button type='button'>Start quiz</button>
+            <button className="start-btn" type='button'>Start quiz</button>
         </>
     )
 }
